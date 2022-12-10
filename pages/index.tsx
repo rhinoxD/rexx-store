bd2a9c5e35736809c835969e362941ddde4ce997
unknown <coolshiv0721@gmail.com>
2022-12-10 14:41:07 +0530

    Infer get static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
-export default function Home() {
-  const message: string = 'Hey!';
+import type { InferGetStaticPropsType } from 'next';
 
-  return <div>{message}</div>;
+export async function getStaticProps() {
+  const products = [1, 2, 3];
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60,
+  };
+}
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <div>{products}</div>;
 }
