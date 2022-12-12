d0e77b367512a8af925f6d11e28333afffd14ded
unknown <coolshiv0721@gmail.com>
2022-12-12 15:12:16 +0530

    Move fetch api

---
+++ framework/shopify/utils/fetch-api.ts
@@ -0,0 +1,30 @@
+type FetchParams = {
+  query: string;
+};
+
+const fetchApi = async ({ query }: FetchParams) => {
+  const url = 'http://localhost:4000/graphql';
+
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+    }),
+  });
+
+  const { data, errors } = await res.json();
+  // ?? is checking if the left hand expression is null
+  // or undefined -> if it is go with the right expression
+  // || is checking if the left hand expression is null,
+  // undefined, "", 0, false
+  if (errors) {
+    throw new Error(errors[0].message ?? errors.message);
+  }
+
+  return { data };
+};
+
+export default fetchApi;
