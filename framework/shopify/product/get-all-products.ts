ebbbecefc05f93d3a7014638f60023926c814aa8
unknown <coolshiv0721@gmail.com>
2022-12-13 14:59:57 +0530

    Module re-exports

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,7 +1,5 @@
-import getAllProductsQuery from '../utils/queries/get-all-products';
-import fetchApi from '../utils/fetch-api';
+import { fetchApi, normalizeProduct, getAllProductsQuery } from '../utils';
 import { ProductConnection } from '../schema';
-import { normalizeProduct } from '../utils/normalize';
 import { Product } from '@common/types/product';
 
 type ReturnType = {
