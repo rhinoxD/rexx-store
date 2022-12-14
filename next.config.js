5410cc637fa45801d73be3ff35f516500ed69e3d
unknown <coolshiv0721@gmail.com>
2022-12-14 14:32:00 +0530

    Framework specific configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const { withFrameworkConfig } = require('./framework/common/config');
+
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
@@ -7,6 +10,6 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withFrameworkConfig(nextConfig);
 
 console.log('next.config.js', JSON.stringify(module.exports, null, 2));
