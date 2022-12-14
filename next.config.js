a7d2a0b94915baf02d63c0350cfa0400473f41f3
unknown <coolshiv0721@gmail.com>
2022-12-14 14:20:35 +0530

    Next config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ['en-US', 'es'],
+    defaultLocale: 'en-US',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+console.log('next.config.js', JSON.stringify(module.exports, null, 2));
