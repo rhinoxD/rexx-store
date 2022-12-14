f5c8fbe24d614a729c75e36151f6c5b2d6e5cb0e
unknown <coolshiv0721@gmail.com>
2022-12-14 14:42:52 +0530

    Ts config specific setup

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const merge = require('deepmerge');
+const fs = require('fs');
 
 function withFrameworkConfig(defaultConfig = {}) {
   const framework = 'shopify';
@@ -10,6 +11,14 @@ function withFrameworkConfig(defaultConfig = {}) {
     'next.config.js'
   ));
   const config = merge(defaultConfig, frameworkNextConfig);
+
+  const tsPath = path.join(process.cwd(), 'tsconfig.json');
+  const tsConfig = require(tsPath);
+
+  tsConfig.compilerOptions.paths['@framework'] = [`framework/${framework}`];
+  tsConfig.compilerOptions.paths['@framework/*'] = [`framework/${framework}/*`];
+
+  fs.writeFileSync(tsPath, JSON.stringify(tsConfig, null, 2));
 
   return config;
 }
