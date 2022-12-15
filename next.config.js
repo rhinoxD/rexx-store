2eb3ca20a10d6f86954ab7329781dc66be0c10bf
unknown <coolshiv0721@gmail.com>
2022-12-15 14:11:50 +0530

    Format JSON in config file

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const { withFrameworkConfig } = require('./framework/common/config');
 
 const nextConfig = {
   reactStrictMode: true,
+  framework: {
+    name: 'shopify',
+  },
   i18n: {
     locales: ['en-US', 'es'],
     defaultLocale: 'en-US',
