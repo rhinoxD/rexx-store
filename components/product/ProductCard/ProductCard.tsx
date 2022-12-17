11f219ed81f69057b4661707d48c615d0935932f
unknown <coolshiv0721@gmail.com>
2022-12-17 13:34:26 +0530

    Product info

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,12 +1,34 @@
 import { Product } from '@common/types/product';
 import { FC } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 interface Props {
   product: Product;
 }
 
+const placeholderImage = '/product-image-placeholder.svg';
+
 const ProductCard: FC<Props> = ({ product }) => {
-  return <div>{product.name}</div>;
+  return (
+    <Link href={`/products/${product.slug}`}>
+      <div>
+        <h3>
+          <span>{product.name}</span>
+        </h3>
+        <span>$14</span>
+      </div>
+      {product.images && (
+        <Image
+          alt={product.name ?? 'Product Image'}
+          src={placeholderImage}
+          height={540}
+          width={540}
+          quality='85'
+        />
+      )}
+    </Link>
+  );
 };
 
 export default ProductCard;
