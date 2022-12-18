243f948edd3091ff491df378c8212211da1a0678
unknown <coolshiv0721@gmail.com>
2022-12-18 13:30:43 +0530

    LayoutA

--- components/ui/Grid/Grid.tsx
+++ components/ui/Grid/Grid.tsx
@@ -1,12 +1,18 @@
 import { FC, ReactNode } from 'react';
 import s from './Grid.module.css';
+import cn from 'classnames';
 
 interface Props {
-  children?: ReactNode;
+  children?: ReactNode[];
+  layout?: 'A' | 'B';
 }
 
-const Grid: FC<Props> = ({ children }) => {
-  return <div className={s.root}>{children}</div>;
+const Grid: FC<Props> = ({ children, layout = 'A' }) => {
+  const rootClassName = cn(s.root, {
+    [s.layoutA]: layout === 'A',
+    [s.layoutB]: layout === 'B',
+  });
+  return <div className={rootClassName}>{children}</div>;
 };
 
 export default Grid;
