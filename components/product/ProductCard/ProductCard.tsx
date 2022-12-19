cab80781e3c69a6824226ec7e9ed606f5cc5f4fc
unknown <coolshiv0721@gmail.com>
2022-12-19 15:04:49 +0530

    Product bg

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -13,6 +13,7 @@ const placeholderImage = '/product-image-placeholder.svg';
 const ProductCard: FC<Props> = ({ product }) => {
   return (
     <Link href={`/products/${product.slug}`} className={s.root}>
+      <div className={s.productBg}></div>
       <div className={s.productTag}>
         <h3 className={s.productTitle}>
           <span>{product.name}</span>
