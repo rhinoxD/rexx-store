3ab2cfdef8f0330a6a67737f8819617b26bfeee9
unknown <coolshiv0721@gmail.com>
2022-12-20 15:25:56 +0530

    Container custom element

--- components/ui/Container/Container.tsx
+++ components/ui/Container/Container.tsx
@@ -2,14 +2,11 @@ import { FC, ReactNode } from 'react';
 
 interface Props {
   children: ReactNode | ReactNode[];
+  el?: any;
 }
 
-const Container: FC<Props> = ({ children }) => {
-  return (
-    <div style={{ maxWidth: '1920px' }} className='px-6 mx-auto'>
-      {children}
-    </div>
-  );
+const Container: FC<Props> = ({ children, el: Component = 'div' }) => {
+  return <Component className='max-w-8xl px-6 mx-auto'>{children}</Component>;
 };
 
 export default Container;
