15b38c09e493ab69c319542d2633ea4e6a421e41
unknown <coolshiv0721@gmail.com>
2022-12-20 15:19:59 +0530

    Container

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import Link from 'next/link';
 
 import s from './Hero.module.css';
+import { Container } from '@components/ui';
 
 interface Props {
   headline: string;
@@ -11,15 +12,17 @@ interface Props {
 const Hero: FC<Props> = ({ headline, description }) => {
   return (
     <div className='bg-black'>
-      <div className={s.root}>
-        <h2 className={s.headline}>{headline}</h2>
-        <div>
-          <p className={s.description}>{description}</p>
-          <Link href='#' className={s.link}>
-            Read it here
-          </Link>
+      <Container>
+        <div className={s.root}>
+          <h2 className={s.headline}>{headline}</h2>
+          <div>
+            <p className={s.description}>{description}</p>
+            <Link href='#' className={s.link}>
+              Read it here
+            </Link>
+          </div>
         </div>
-      </div>
+      </Container>
     </div>
   );
 };
