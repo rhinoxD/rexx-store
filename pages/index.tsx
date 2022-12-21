e6434d28e46d52206772088b8120fe70a51c252f
unknown <coolshiv0721@gmail.com>
2022-12-21 14:22:14 +0530

    Marquee

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import getAllProducts from '@framework/product/get-all-products';
 import { getConfig } from '@framework/api/config';
 import { Layout } from '@components/common';
 import { ProductCard } from '@components/product';
-import { Grid, Hero } from '@components/ui';
+import { Grid, Hero, Marquee } from '@components/ui';
 
 export async function getStaticProps() {
   const config = getConfig();
@@ -31,6 +31,11 @@ export default function Home({
         headline='Cookies, ice creams and muffins'
         description='Topping lemon drops candy canes cheesecake cotton candy jujubes donut gummi bears. Muffin shortbread pudding cheesecake chocolate candy gingerbread gummi bears. Pudding toffee oat cake powder lemon drops carrot cake. Brownie cake pastry muffin candy canes. Toffee toffee soufflé sweet fruitcake danish fruitcake jelly beans biscuit. Caramels macaroon carrot cake toffee bonbon danish shortbread. Pie tart powder chupa chups shortbread soufflé toffee muffin. Biscuit cotton candy lollipop sweet roll candy topping wafer. Tiramisu brownie shortbread cookie pie powder jelly beans sesame snaps. Tiramisu carrot cake ice cream gummies icing ice cream. Tart biscuit dragée sweet roll gummies. Halvah shortbread cake biscuit bonbon caramels macaroon sweet.'
       />
+      <Marquee>
+        {products.slice(0, 3).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </Marquee>
     </>
   );
 }
