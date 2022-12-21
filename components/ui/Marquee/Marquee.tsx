882ab7679bd794a8edfb2b8e88b5f45d6444a364
unknown <coolshiv0721@gmail.com>
2022-12-21 15:17:25 +0530

    Marquee variant

--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -1,15 +1,20 @@
 import { FC, ReactNode } from 'react';
 import Ticker from 'react-ticker';
+import cn from 'classnames';
 
 import s from './Marquee.module.css';
 
 interface Props {
   children: ReactNode[];
+  variant?: 'primary' | 'secondary';
 }
 
-const Marquee: FC<Props> = ({ children }) => {
+const Marquee: FC<Props> = ({ children, variant = 'primary' }) => {
+  const rootClassName = cn(s.root, {
+    [s.secondary]: variant === 'secondary',
+  });
   return (
-    <div className={s.root}>
+    <div className={rootClassName}>
       <Ticker offset={80}>
         {() => <div className={s.container}>{children}</div>}
       </Ticker>
