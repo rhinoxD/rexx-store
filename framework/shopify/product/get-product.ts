1d8fc1db4ab0bf6d1f1ccb69ca04e7184be2fc8d
unknown <coolshiv0721@gmail.com>
2022-12-24 14:33:49 +0530

    Get normalized product

--- framework/shopify/product/get-product.ts
+++ framework/shopify/product/get-product.ts
@@ -1,29 +1,30 @@
 import { ApiConfig, Variables } from '@common/types/api';
-import { getProductQuery } from '@framework/utils';
+import { getProductQuery, normalizeProduct } from '@framework/utils';
 
 import { Product as ShopifyProduct } from '@framework/schema';
+import { Product } from '@common/types/product';
 
 type FetchType = {
   productByHandle: ShopifyProduct;
 };
 
+type ReturnType = {
+  product: Product | null;
+};
+
 const getProduct = async (options: {
   config: ApiConfig;
   variables: Variables;
-}): Promise<any> => {
+}): Promise<ReturnType> => {
   const { config, variables } = options;
   const { data } = await config.fetch<FetchType>({
     query: getProductQuery,
     url: config.apiUrl,
     variables,
   });
-
-  console.log(JSON.stringify(data.productByHandle, null, 2));
+  const { productByHandle } = data;
   return {
-    product: {
-      name: 'My super product',
-      slug: 'my-super-product',
-    },
+    product: productByHandle ? normalizeProduct(productByHandle) : null,
   };
 };
 
--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -26,7 +26,7 @@ export const getStaticProps = async ({
 
   const { product } = await getProduct({
     config,
-    variables: { slug: params!.slug },
+    variables: { slug: params?.slug },
   });
   return {
     props: {
@@ -38,12 +38,7 @@ export const getStaticProps = async ({
 export default function ProductSlug({
   product,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return (
-    <div>
-      {product.name}
-      {product.slug}
-    </div>
-  );
+  return <div>{JSON.stringify(product, null, 2)}</div>;
 }
 
 ProductSlug.Layout = Layout;
