c2304a67410df61daba715e2779bb156e6c300cc
unknown <coolshiv0721@gmail.com>
2022-12-24 13:26:33 +0530

    Get product query

--- framework/shopify/product/get-product.ts
+++ framework/shopify/product/get-product.ts
@@ -1,6 +1,13 @@
 import { ApiConfig } from '@common/types/api';
+import { getProductQuery } from '@framework/utils';
 
 const getProduct = async (config: ApiConfig): Promise<any> => {
+  const { data } = await config.fetch<any>({
+    query: getProductQuery,
+    url: config.apiUrl,
+  });
+
+  console.log(JSON.stringify(data, null, 2));
   return {
     product: {
       name: 'My super product',
