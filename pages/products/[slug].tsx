2e818cea6860818ddf155c6ba909d19a70025c5a
unknown <coolshiv0721@gmail.com>
2022-12-26 13:08:39 +0530

    Product view

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -7,7 +7,7 @@ import {
 import { Layout } from '@components/common';
 import { getConfig } from '@framework/api/config';
 import { getAllProductsPaths, getProduct } from '@framework/product';
-import { Container } from '@components/ui';
+import { ProductView } from '@components/product';
 
 // fetch all of the product slugs
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -39,52 +39,7 @@ export const getStaticProps = async ({
 export default function ProductSlug({
   product,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return (
-    <Container>
-      <div>
-        <p>id: {product?.id}</p>
-        <p>name: {product?.name}</p>
-        <p>price value: {product?.price.value}</p>
-        <p>price currency: {product?.price.currencyCode}</p>
-        <p>description: {product?.description}</p>
-
-        <h1 className='my-4 text-xl'>OPTIONS</h1>
-        <div>
-          {product?.options.map((option) => (
-            <div>
-              <p>Name: {option.displayName}</p>
-              {option.values.map((value) => (
-                <div>
-                  <p>Label: {value.label}</p>
-                  <p>Hex Color: {value.hexColor}</p>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-
-        <h1 className='my-4 text-xl'>VARIANTS</h1>
-        <div>
-          {product?.variants.map((variant) => (
-            <div>
-              <p>Variant Name: {variant.name}</p>
-              {variant.options.map((vo) => (
-                <div>
-                  <p>Name: {vo.displayName}</p>
-                  {vo.values.map((value) => (
-                    <div>
-                      <p>Label: {value.label}</p>
-                      <p>Hexcolot: {value.hexColor}</p>
-                    </div>
-                  ))}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-      </div>
-    </Container>
-  );
+  return <>{product && <ProductView product={product} />}</>;
 }
 
 ProductSlug.Layout = Layout;
