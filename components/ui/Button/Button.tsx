3ae7666a299a731eec4da4c4c5512f48e9ae5025
unknown <coolshiv0721@gmail.com>
2022-12-27 13:34:18 +0530

    UI button

---
+++ components/ui/Button/Button.tsx
@@ -0,0 +1,16 @@
+import { FC, ReactNode } from 'react'
+import s from './Button.module.css'
+
+interface Props {
+  children: ReactNode | ReactNode[]
+}
+
+const Button: FC<Props> = ({ children }) => {
+  return (
+    <button className={s.root} type='button'>
+      {children}
+    </button>
+  )
+}
+
+export default Button
