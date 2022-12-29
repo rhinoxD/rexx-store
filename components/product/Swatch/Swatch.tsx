e06c9651535c56cf969407cfbf5c4ee67aca85df
unknown <coolshiv0721@gmail.com>
2022-12-29 12:51:23 +0530

    isDark color

--- components/product/Swatch/Swatch.tsx
+++ components/product/Swatch/Swatch.tsx
@@ -3,6 +3,7 @@ import cn from 'classnames'
 
 import s from './Swatch.module.css'
 import { Check } from '@components/icons'
+import { isDark } from '@lib/color'
 
 interface Props {
   color?: string
@@ -20,6 +21,7 @@ const Swatch: FC<Props> = ({ color, label, variant, active, ...rest }) => {
     [s.active]: active,
     [s.color]: color,
     [s.size]: variant === 'size',
+    [s.dark]: color && isDark(color),
   })
 
   return (
