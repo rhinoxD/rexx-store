b826ed1b11dc82ce4cc32967eff1a8a1b054fe91
unknown <coolshiv0721@gmail.com>
2022-12-31 10:11:37 +0530

    Use api provider

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -5,6 +5,7 @@ import s from './Layout.module.css';
 import { CartSidebar } from '@components/cart';
 import { Sidebar } from '@components/ui';
 import { useUI } from '@components/ui/context';
+import { ApiProvider } from '@common';
 
 interface Props {
   children?: ReactNode;
@@ -13,14 +14,16 @@ interface Props {
 const Layout: FC<Props> = ({ children }) => {
   const { isSidebarOpen, closeSidebar } = useUI();
   return (
-    <div className={s.root}>
-      <Navbar />
-      <Sidebar onClose={closeSidebar} isOpen={isSidebarOpen}>
-        <CartSidebar />
-      </Sidebar>
-      <main className='fit'>{children}</main>
-      <Footer />
-    </div>
+    <ApiProvider>
+      <div className={s.root}>
+        <Navbar />
+        <Sidebar onClose={closeSidebar} isOpen={isSidebarOpen}>
+          <CartSidebar />
+        </Sidebar>
+        <main className='fit'>{children}</main>
+        <Footer />
+      </div>
+    </ApiProvider>
   );
 };
 
--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -9,6 +9,7 @@ import { ProductSlider, Swatch } from '@components/product'
 import { Choices, getVariant } from '../helpers'
 import { useUI } from '@components/ui/context'
 import useAddItem from '@framework/cart/use-add-item'
+import { useApiProvider } from '@common'
 
 interface Props {
   product: Product
@@ -16,6 +17,7 @@ interface Props {
 
 const ProductView: FC<Props> = ({ product }) => {
   const [choices, setChoices] = useState<Choices>({})
+  const api = useApiProvider()
   const { openSidebar } = useUI()
   const addItem = useAddItem()
 
