ce0e1e90cd3d3ea6405b22a2df4226d23f5df9c1
unknown <coolshiv0721@gmail.com>
2023-01-01 09:24:02 +0530

    Api hooks

--- framework/common/types/api.ts
+++ framework/common/types/api.ts
@@ -1,16 +1,22 @@
 export type ApiFetcherOptions = {
-  url: string;
-  query: string;
-  variables?: Variables;
-};
+  url: string
+  query: string
+  variables?: Variables
+}
 
-export type Variables = { [key: string]: string | undefined };
+export type Variables = { [key: string]: string | undefined }
 
 export type ApiFetcherResults<T> = {
-  data: T;
-};
+  data: T
+}
 
 export interface ApiConfig {
-  apiUrl: string;
-  fetch<T>(options: ApiFetcherOptions): Promise<ApiFetcherResults<T>>;
+  apiUrl: string
+  fetch<T>(options: ApiFetcherOptions): Promise<ApiFetcherResults<T>>
+}
+
+export interface ApiHooks {
+  cart: {
+    useAddItem: any
+  }
 }
--- framework/shopify/index.tsx
+++ framework/shopify/index.tsx
@@ -4,6 +4,7 @@ import {
   ApiProvider as CoreApiProvider,
   useApiProvider as useCoreApiProvider,
 } from '@common'
+import { shopifyHooks } from './hooks'
 import { getConfig } from './api/config'
 
 const config = getConfig()
@@ -13,7 +14,11 @@ interface ShopifyApiProviderProps {
 }
 
 export const ApiProvider = ({ children }: ShopifyApiProviderProps) => {
-  return <CoreApiProvider config={{ ...config }}>{children}</CoreApiProvider>
+  return (
+    <CoreApiProvider config={{ ...config }} hooks={shopifyHooks}>
+      {children}
+    </CoreApiProvider>
+  )
 }
 
 export const useApiProvider = () => useCoreApiProvider()
