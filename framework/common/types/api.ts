91b738f172a67be271966a0a4a7ea3281ca81e08
unknown <coolshiv0721@gmail.com>
2023-01-02 11:40:24 +0530

    Fix fetcher type

--- framework/common/types/api.ts
+++ framework/common/types/api.ts
@@ -10,7 +10,7 @@ export type ApiFetcherResults<T> = {
 }
 
 export interface ApiConfig {
-  fetch: ApiFetcher
+  fetch<T>(options: ApiFetcherOptions): Promise<ApiFetcherResults<T>>
 }
 
 export interface ApiHooks {
