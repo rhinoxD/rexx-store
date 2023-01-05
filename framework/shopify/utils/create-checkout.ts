b7aa9c2063a1c2a074eb6c1b0210940ff1028df0
unknown <coolshiv0721@gmail.com>
2023-01-05 11:09:20 +0530

    Store cookies to browser

--- framework/shopify/utils/create-checkout.ts
+++ framework/shopify/utils/create-checkout.ts
@@ -1,4 +1,11 @@
+import Cookies from 'js-cookie'
+
 import { ApiFetcher } from '@common/types/api'
+import {
+  SHOPIFY_CHECKOUT_ID_COOKIE,
+  SHOPIFY_CHECKOUT_URL_COOKIE,
+  SHOPIFY_COOKIE_EXPIRE,
+} from '@framework/const'
 import { Checkout, CheckoutCreatePayload, Maybe } from '@framework/schema'
 import { checkoutCreateMutation } from './mutations'
 
@@ -10,6 +17,15 @@ const createCheckout = async (
   })
   const { checkout } = data.checkoutCreate
   const checkoutId = checkout?.id
+
+  if (checkoutId) {
+    const options = {
+      expires: SHOPIFY_COOKIE_EXPIRE,
+    }
+
+    Cookies.set(SHOPIFY_CHECKOUT_ID_COOKIE, checkoutId, options)
+    Cookies.set(SHOPIFY_CHECKOUT_URL_COOKIE, checkout?.webUrl, options)
+  }
 
   return checkout
 }
