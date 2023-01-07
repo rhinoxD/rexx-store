937be3633aa9daaabf4357cdae3f5e0508b448d3
unknown <coolshiv0721@gmail.com>
2023-01-07 11:56:35 +0530

    Cart normalization

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -8,9 +8,23 @@ import {
   ProductVariantConnection,
   SelectedOption,
 } from '../schema'
+import { Cart } from '@common/types/cart'
 
-export const normalizeCart = (checkout: Checkout): any => {
-  return checkout
+export const normalizeCart = (checkout: Checkout): Cart => {
+  return {
+    id: checkout.id,
+    createdAt: checkout.createdAt,
+    currency: {
+      code: checkout.totalPriceV2.currencyCode,
+    },
+    taxesIncluded: checkout.taxesIncluded,
+    lineItemsSubtotalPrice: +checkout.subtotalPriceV2.amount,
+    totalPrice: checkout.totalPriceV2.amount,
+    lineItems: checkout.lineItems.edges.map(
+      (lineItemEdge) => lineItemEdge.node
+    ),
+    discounts: [],
+  }
 }
 
 const normalizeProductImages = ({ edges }: { edges: Array<ImageEdge> }) =>
