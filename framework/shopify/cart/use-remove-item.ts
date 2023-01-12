669c4ea240396690104bbe8f476af10d0ed03b3f
unknown <coolshiv0721@gmail.com>
2023-01-12 12:14:44 +0530

    Remove item done

--- framework/shopify/cart/use-remove-item.ts
+++ framework/shopify/cart/use-remove-item.ts
@@ -2,7 +2,9 @@ import useRemoveItem from '@common/cart/use-remove-item'
 import { Cart } from '@common/types/cart'
 import { MutationHook } from '@common/types/hooks'
 import { CheckoutLineItemsRemovePayload } from '@framework/schema'
-import { getCheckoutId } from '@framework/utils'
+import { checkoutToCart, getCheckoutId } from '@framework/utils'
+import { checkoutLineItemsRemoveMutation } from '@framework/utils/mutations'
+import useCart from './use-cart'
 
 export default useRemoveItem
 
@@ -18,24 +20,27 @@ export type RemoveItemDescriptor = {
 
 export const handler: MutationHook<RemoveItemDescriptor> = {
   fetcherOptions: {
-    query: 'query { hello }',
+    query: checkoutLineItemsRemoveMutation,
   },
   async fetcher({ input: { id }, options, fetch }) {
     const { data } = await fetch({
       ...options,
       variables: {
         checkoutId: getCheckoutId(),
-        lineItemsIds: [id],
+        lineItemIds: [id],
       },
     })
 
-    return (data + '_modified!!!!') as any
+    const cart = checkoutToCart(data.checkoutLineItemsRemove.checkout)
+    return cart
   },
   useHook:
     ({ fetch }) =>
     () => {
+      const { mutate: updateCart } = useCart()
       return async (input) => {
         const data = await fetch(input)
+        updateCart(data, false)
         return data
       }
     },
