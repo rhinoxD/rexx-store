eb4a1c7c4919b2eb6789037985d2d3d85c48f296
Shivam Kumar <coolshiv0721@gmail.com>
2023-01-13 11:11:05 +0530

    Quantity improvement

--- components/cart/CartItem/CartItem.tsx
+++ components/cart/CartItem/CartItem.tsx
@@ -23,8 +23,7 @@ const CartItem = ({
   const price = item.variant.price! * item.quantity || 0
   const { options } = item
 
-  const handleQuantity = async (e: ChangeEvent<HTMLInputElement>) => {
-    const val = Number(e.target.value)
+  const handleQuantityChange = async (val: number) => {
     if (Number.isInteger(val) && val >= 0) {
       setQuantity(val)
       await updateItem({
@@ -35,16 +34,14 @@ const CartItem = ({
     }
   }
 
+  const handleQuantity = async (e: ChangeEvent<HTMLInputElement>) => {
+    const val = Number(e.target.value)
+    handleQuantityChange(val)
+  }
+
   const incrementQuantity = async (n = 1) => {
     const val = Number(quantity) + n
-    if (Number.isInteger(val) && val >= 0) {
-      setQuantity(val)
-      await updateItem({
-        id: item.id,
-        variantId: item.variantId,
-        quantity: val,
-      })
-    }
+    handleQuantityChange(val)
   }
 
   return (
