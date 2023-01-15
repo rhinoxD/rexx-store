44d52a313949fbe771bb187e4b78cd99d743dd36
Shivam Kumar <coolshiv0721@gmail.com>
2023-01-15 11:00:39 +0530

    Accessing storefront API

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -67,7 +67,10 @@ const normalizeLineItem = ({
 
 const normalizeProductImages = ({ edges }: { edges: Array<ImageEdge> }) =>
   edges.map(({ node: { originalSrc: url, ...rest } }) => ({
-    url: `/images/${url}`,
+    url:
+      process.env.NEXT_PUBLIC_FRAMEWORK === 'shopify_local'
+        ? `/images/${url}`
+        : url ?? '/product-image-placeholder.svg',
     ...rest,
   }))
 
